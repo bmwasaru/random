566b357c85528df9fa1d9ad432b11e216206dd49
Brighton Mwasaru <bmwasaru@gmail.com>
2016-01-23 20:43:45 +0300

    Upgrade multiple packages in a virtual environ

---
+++ pip_upgrade_packages.py
@@ -0,0 +1,7 @@
+"""Upgrade all the packages in a virtual environment"""
+
+import pip
+from subprocess import call
+
+for dist in pip.get_installed_distributions():
+    call("pip install --upgrade " + dist.project_name, shell=True)
