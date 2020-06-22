cae0c6879eff2aebcc42ec3490802819b6a6b661
Britone Mwasaru <bmwasaru@gmail.com>
2020-06-22 12:21:48 +0300

    add shebang

--- git_pull.sh
+++ git_pull.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # this is handing if you say have clone many repos in a single directory and want to do a git pull on all the repos
 
 for i in */.git; do ( echo $i; cd $i/..; git pull; ); done
