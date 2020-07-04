f91c1b70afabbe3021e1be7bfe518e2def25d480
Britone Mwasaru <bmwasaru@gmail.com>
2020-07-04 05:55:35 +0300

    Generating a 2048 bit RSA private key command

--- simple_http_server.py
+++ simple_http_server.py
@@ -12,5 +12,6 @@ except:
 
 if __name__ == '__main__':
     httpd = HTTPServer(('', 443), SimpleHTTPRequestHandler)
+    # openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout key.pem
     httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='./cert.pem', keyfile='./key.pem', ssl_version=ssl.PROTOCOL_TLSv1)
     httpd.serve_forever()
