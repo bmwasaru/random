0f269b05e1d97abca980a44fb5540e202ed404fe
Britone Mwasaru <bmwasaru@gmail.com>
2020-11-25 07:50:50 +0300

    Create remove_checkout_fields.php

---
+++ remove_checkout_fields.php
@@ -0,0 +1,15 @@
+/* WooCommerce: Removes Checkout Fields */
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+function custom_override_checkout_fields( $fields ) {
+unset($fields['billing']['billing_company']);
+unset($fields['billing']['billing_address_1']);
+unset($fields['billing']['billing_address_2']);
+unset($fields['billing']['billing_city']);
+unset($fields['billing']['billing_postcode']);
+unset($fields['billing']['billing_country']);
+unset($fields['billing']['billing_state']);
+unset($fields['account']['account_username']);
+unset($fields['account']['account_password']);
+unset($fields['account']['account_password-2']);
+return $fields;
+}
